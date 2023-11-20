fc53983acb700eaff91c7acbc5a7a41580eeb087
karunanayakedinura <128465343+karunanayakedinura@users.noreply.github.com>
2023-11-20 15:02:18 +0100

    Update LigueConsole.java Changer administrateur

--- Personnel/src/commandLine/LigueConsole.java
+++ Personnel/src/commandLine/LigueConsole.java
@@ -72,7 +72,7 @@ public class LigueConsole
 		Menu menu = new Menu("Editer " + ligue.getNom());
 		menu.add(afficher(ligue));
 		menu.add(gererEmployes(ligue));
-		//menu.add(changerAdministrateur(ligue));
+		menu.add(changerAdministrateur(ligue));
 		menu.add(changerNom(ligue));
 		menu.add(supprimer(ligue));
 		menu.addBack("q");
@@ -127,7 +127,10 @@ public class LigueConsole
 	
 	private List<Employe> changerAdministrateur(final Ligue ligue)
 	{
-		return null;
+		return new List<>("Employé à avoir le statut d'administrateur", "a", 
+			() -> new ArrayList<>(ligue.getEmployes()),
+			(index, element) -> {ligue.setAdministrateur(element);}
+			);
 	}		
 
 	private List<Employe> modifierEmploye(final Ligue ligue)
